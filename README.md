c89663b6bb3430c75a661fba8275d4b5c59171f5
Jakub Nowakowski <jakub.nowakowski.home@outlook.com>
2017-02-01 23:45:59 +0100

    new

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # strategiesandtrades
+
+
+"server": "nodemon --watch server ",
+
+"server": "babel-node server/index.js",
+
+
+
---
+++ src/Components/App/NavigationBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default () => {
+    return(
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">Red Dice</a>
+          </div>
+
+          <div className="collapse navbar-collapse">
+            <ul className="nav nacbar-nav navbar-right">
+                <li><a href="#">Sign up</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+}
