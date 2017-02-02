d6a2f363567ff184619bcb4822a28cdffdd8ec6a
Jakub <qba.nowakowski@gmail.com>
2017-02-02 10:36:41 +0100

    client update

--- src/client.js
+++ src/client.js
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom';
 import Layout from "./components/app/Layout";
 //import './index.css';
 
+import Archives from "./pages/Archives";
+import Featured from "./pages/Featured";
+import Layout from "./pages/Layout";
+import Settings from "./pages/Settings";
+
 ReactDOM.render(
   <Layout />,
   document.getElementById('app')
