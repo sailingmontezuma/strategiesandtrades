18e33aa82d701c6cbc602bed90492506aca3ea70
Jakub <qba.nowakowski@gmail.com>
2017-02-02 16:18:22 +0100

    ...

--- src/client.js
+++ src/client.js
@@ -4,12 +4,30 @@ import ReactDOM from 'react-dom';
 import Layout from "./components/app/Layout";
 //import './index.css';
 
+ 
+import { Router, Route, Link, browserHistory } from 'react-router'
+
 import Archives from "./pages/Archives";
 import Featured from "./pages/Featured";
 import Layout from "./pages/Layout";
 import Settings from "./pages/Settings";
 
 ReactDOM.render(
-  <Layout />,
+  //<Layout />,
+  <Router history={browserHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Featured}></IndexRoute>
+      <Route path="archives(/:article)" name="archives" component={Archives}></Route>
+      <Route path="settings" name="settings" component={Settings}></Route>
+      <Route path="*" component={NoMatch}/>
+    </Route>
+  </Router>
+,
   document.getElementById('app')
 );
+
+/*
+<Route path="users" component={Users}>
+  <Route path="/user/:userId" component={User}/>
+</Route>
+*/
